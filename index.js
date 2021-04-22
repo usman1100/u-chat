cb28388fe53c8e7b4fe9110c41161fa13c02ab8e
usman <usmanahmed1100@gmail.com>
2021-04-23 04:50:14 +0500

    Connected local mongoDB

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const app = require("express")()
+const mongoose = require("mongoose")
+
 
 const port = process.env.PORT || 2021
 
-app.listen(port, ()=>{
-    console.log("Started on http://localhost:" + port);
+
+
+mongoose.connect("mongodb://localhost:27017/uchat", {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+}, ()=>{
+    console.log("DB connected");
+    app.listen(port, ()=>{
+        console.log("Started on http://localhost:" + port);
+    })
 })
+
+
 
 
 app.get("/", (req, res)=>{
