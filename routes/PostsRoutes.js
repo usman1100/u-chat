71fe818eabe04c2e6305cdfb9b5422c709ccb6e8
usman <usmanahmed1100@gmail.com>
2021-04-25 07:03:55 +0500

    Fetching posts by username works

--- routes/PostsRoutes.js
+++ routes/PostsRoutes.js
@@ -2,25 +2,36 @@ const Router = require("express").Router();
 const Post = require("../models/PostModel")
 
 
-Router.get("/all", async  (req, res)=>{
-    try
-    {
+Router.get("/all", async (req, res) => {
+    try {
         const data = await Post.find();
         return res.json(data);
     }
 
-    catch(err){
+    catch (err) {
         return res.json(err).status(501);
     }
 })
 
 
-Router.get("/specific/:PostID", (req, res) => {
-    res.json({"PostID":req.params.PostID})
+Router.get("/postedby/:username", async (req, res) => {
+    try {
+        const username = req.params.username;
+        const data = await Post.find({ postedBy: username });
+
+        if (data.length === 0)
+            return res.json("no posts by this username found");
+        else
+            return res.json(data)
+
+    } 
+    catch (error) {
+        return res.json(error).status(501);
+    }
 })
 
-Router.get("/create", (req, res)=>{
-    if(req.session.user){
+Router.get("/create", (req, res) => {
+    if (req.session.user) {
         return res.render("newPost");
     }
 
@@ -28,28 +39,28 @@ Router.get("/create", (req, res)=>{
 })
 
 
-Router.post("/create/new", async (req, res)=>{
+Router.post("/create/new", async (req, res) => {
 
-    if(!req.session.user){
+    if (!req.session.user) {
         return res.json("You are not logged in").status(401);
     }
 
-    const {title, body} = req.body;
+    const { title, body } = req.body;
     const postedBy = req.session.user.username;
 
 
     const newPost = Post({
-        title:title,
-        body:body,
-        postedBy:postedBy
+        title: title,
+        body: body,
+        postedBy: postedBy
     })
 
-    try{
+    try {
         const data = await newPost.save();
         return res.json("Success");
     }
 
-    catch(err){
+    catch (err) {
         return res.json(err).status(501);
     }
 
